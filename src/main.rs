ba8160cd253021306f82263b91a0c1f8cffe4ade
reidspreiter <reidspreiter@gmail.com>
2024-05-26 14:45:33 -0500

    [FT]: Link index.html file

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,9 @@
-// mod enigma;
-// use enigma::Enigma;
+mod enigma;
+use enigma::Enigma;
 
 use actix_files::NamedFile;
-use actix_web::{get, web, App, HttpRequest, HttpServer};
+use actix_web::{get, App, HttpRequest, HttpServer};
 use std::path::PathBuf;
-
-// fn main() {
-//     let mut enigma = Enigma::new(
-//         "abghfi",
-//         "UKWA",
-//         "II", 23,
-//         "I", 1,
-//         "V", 0,
-//     );
-//     let plaintext = "She sat deep in thought, for the room was on fire!";
-//     let ciphertext = enigma.encode(plaintext, false, true, true);
-//     println!("{}", ciphertext);
-
-//     let mut enigma = Enigma::new(
-//         "abghfi",
-//         "UKWA",
-//         "II", 23,
-//         "I", 1,
-//         "V", 0,
-//     );
-//     let should_be_plain = enigma.encode(&ciphertext, false, true, true);
-//     assert_eq!(plaintext, should_be_plain);
-//     println!("{}", should_be_plain);
-// }
 
 #[get("/")]
 async fn index(_req: HttpRequest) -> actix_web::Result<NamedFile> {
